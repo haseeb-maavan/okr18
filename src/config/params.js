7e6034df234ce1d593895f63c4e0b093ca38430e
Eagle Nest <Eagle Nest@Eagle>
2018-02-26 12:57:10 +0500

    params: api link change

--- src/config/params.js
+++ src/config/params.js
@@ -1,3 +1,3 @@
 export const username = 'maavan';
 export const password = 'test123';
-export const apiurl = 'http://tuckshop.maavan.com/public/api/';
+export const apiurl = 'http://team.maavan.com/public/api/';//'http://tuckshop.maavan.com/public/api/';
