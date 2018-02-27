e0b7ca76817e9a26052aa67c59ff8cad65441291
Eagle Nest <Eagle Nest@Eagle>
2018-02-27 10:59:22 +0500

    Date issue fix

--- src/components/UserLogs.js
+++ src/components/UserLogs.js
@@ -41,7 +41,7 @@ class UserLogs extends Component {
         action : 'GET',
         search: '?tokenID='+ row._id,
         pathname: '/edit-record',
-        state: { recordId: row._id, clientId: row.userId, status: row.status, date: moment(row.date).format('Y-M-D h:mm A'), clientName: this.props.clientName }
+        state: { recordId: row._id, clientId: row.userId, status: row.status, date: moment(row.date.date).format('Y-M-D h:mm A'), clientName: this.props.clientName }
       });
   }
 
@@ -63,11 +63,11 @@ class UserLogs extends Component {
           <tbody>
              { this.state.logs.map((item, index) => (
                  
-                <tr key = {item.status + item.date}>
+                <tr key = {item.status + item.date.date}>
                     <td key = {item.userId}>{this.props.clientName}</td>
                     <td>{this.renderStatus(item.status)}</td>
-                    <td>{moment(item.date).format('hh:mm A')}</td>
-                    <td>{moment(item.date).format('M/D/Y')}</td>
+                    <td>{moment(item.date.date).format('hh:mm A')}</td>
+                    <td>{moment(item.date.date).format('M/D/Y')}</td>
                     <td>
                         <button type="button" className="btn btn-default btn-sm" onClick={this.editRecord.bind(this,item)}>
                             <span className="glyphicon glyphicon-edit"></span> Edit
